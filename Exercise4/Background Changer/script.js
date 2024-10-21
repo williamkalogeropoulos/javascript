e99ecb3f3cf6a40d6a701653da6ae9d37573a9d7
William Kalogeropoulos <williamkalogeropoulos@gmail.com>
2024-10-21 14:55:52 +0300

    Delete Exercise4 directory

--- Exercise4/Background Changer/script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $('#change-color').click(function () {
-        const randomColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-        $('body').css('background-color', randomColor);
-    });
-});
